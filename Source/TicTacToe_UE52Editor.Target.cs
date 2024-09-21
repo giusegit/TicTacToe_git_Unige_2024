04e9870ac650122337f7a7c33ea498011afbf17e
giusegit <37208205+giusegit@users.noreply.github.com>
2024-09-21 16:38:30 +0200

    Update TicTacToe_UE52Editor.Target.cs

--- Source/TicTacToe_UE52Editor.Target.cs
+++ Source/TicTacToe_UE52Editor.Target.cs
@@ -8,8 +8,8 @@ public class TicTacToe_UE52EditorTarget : TargetRules
 	public TicTacToe_UE52EditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("TicTacToe_UE52");
 	}
 }
